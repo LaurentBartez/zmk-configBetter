355b1877d7a036b646301e66e36aa48d6a256637
LaurentBartez <post.thomas@familieknorr.de>
2024-07-13 17:05:32 +0200

    added brackets

--- config/zmk-helpers/helper.h
+++ config/zmk-helpers/helper.h
@@ -149,7 +149,7 @@
                 compatible = "zmk,behavior-mod-morph"; \
                 #binding-cells = <0>; \
                 bindings = <uc_binding shifted_uc_binding>; \
-                mods = <MOD_LSFT|MOD_RSFT>; \
+                mods = <(MOD_LSFT|MOD_RSFT)>; \
             }; \
         }; \
     };
